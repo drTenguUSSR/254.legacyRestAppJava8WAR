75f0374b64a060813f95263503ec66a459e85375
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2025-10-08 22:14:13 +0300

    demo: сбор информации о локальных IPv4 адресах; примеры эмодзи в коде

--- src/test/java/mil/teng254/legacy/controller/MicroLanguageTests.java
+++ src/test/java/mil/teng254/legacy/controller/MicroLanguageTests.java
@@ -1,11 +1,18 @@
 package mil.teng254.legacy.controller;
 
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.*;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class MicroLanguageTests {
     @Test
     public void testRegExpDate() {
@@ -32,17 +39,79 @@ public class MicroLanguageTests {
 
     @Test
     public void specCharTest() {
-        String message="spec-symbols:\0,␀,\u2401,\u2426,\u274C,\u274E,\u2049";
-        message+=",\u27B0"; // Curly Loop, '➰'
-        message+=",\u00BF"; //Inverted Question Mark, '¿'
+        String message = "spec-symbols:\0,␀,\u2401,\u2426,\u274C,\u274E,\u2049";
+        message += ",\u27B0"; // Curly Loop, '➰'
+        message += ",\u00BF"; //Inverted Question Mark, '¿'
         //(UTF-16 Encoding), see https://www.compart.com/en/unicode/block/U+1F300
-        message+=",\uD83D\uDE2D"; //Loudly Crying Face Emoji Meaning,😭
-        message+=",\uD83D\uDC0D"; // Snake,🐍
-        message+=",\uD83D\uDD25"; // Fire,1F525,'🔥'
-        message+=",\uD83D\uDDD1"; // Wastebasket,1F5D1,'🗑'
-        message+=",\u2622\ufe0f"; // Эмодзи Радиация, '☢'
-        message+=",\u2623\ufe0f"; // Эмодзи Биологическая Угроза, '☣️'
-        System.out.println(message);
+        message += ",\uD83D\uDE2D"; //Loudly Crying Face Emoji Meaning,😭
+        message += ",\uD83D\uDC0D"; // Snake,🐍
+        message += ",\uD83D\uDD25"; // Fire,1F525,'🔥'
+        message += ",\uD83D\uDDD1"; // Wastebasket,1F5D1,'🗑'
+        message += ",\u2622\ufe0f"; // Эмодзи Радиация, '☢'
+        message += ",\u2623\ufe0f"; // Эмодзи Биологическая Угроза, '☣️'
+        log.debug("message:{}", message);
+    }
+
+    @Test
+    public void getHostInfo() {
+        HostInfo hostInfo = new HostInfo();
+        String result = "host: " + hostInfo.getName() + ", addresses: " + hostInfo.getAddresses();
+        log.debug("result: {}", result);
+    }
+
+
+    public static class HostInfo {
+        private static final String PREFIX = "HostInfo:";
+
+
+        public String getName() {
+            String host;
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                host = System.getenv("COMPUTERNAME");
+            } else {
+                host = System.getenv("HOSTNAME");
+            }
+            if (Strings.isNullOrEmpty(host)) {
+                try {
+                    InetAddress xlocal = InetAddress.getLocalHost();
+                    host = xlocal.getHostName();
+                } catch (UnknownHostException e) {
+                    log.error(PREFIX + "UnknownHostException", e);
+                }
+            }
+            if (Strings.isNullOrEmpty(host)) {
+                host = "unknown-host";
+            }
+            return host;
+        }
+
+        public String getAddresses() {
+            String addresses;
+            try {
+                Enumeration<NetworkInterface> ifs = NetworkInterface.getNetworkInterfaces();
+                Set<String> ifList = new HashSet<>();
+                while (ifs.hasMoreElements()) {
+                    NetworkInterface ni = ifs.nextElement();
+                    Enumeration<InetAddress> niAddresses = ni.getInetAddresses();
+                    if (ni.isLoopback() || !ni.isUp()) {
+                        continue;
+                    }
+                    while (niAddresses.hasMoreElements()) {
+                        InetAddress addr = niAddresses.nextElement();
+                        if (addr.isLoopbackAddress()) {
+                            continue;
+                        }
+                        if (addr instanceof Inet4Address) {
+                            ifList.add(addr.getHostAddress());
+                        }
+                    }
+                }
+                addresses = String.join(",", ifList);
+            } catch (SocketException e) {
+                addresses = "unknown-addresses";
+            }
+            return addresses;
+        }
     }
 }
 
