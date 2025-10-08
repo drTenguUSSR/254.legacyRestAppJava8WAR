501f53fd9438ab03e80815f9d98f85204bc25c42
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2025-10-08 23:37:51 +0300

    документация: правка по markdownlint в docs-dev\01.multuports.md и docs-dev\02.main-generator-prompt.md

--- markdown-check.cmd
+++ markdown-check.cmd
@@ -1,1 +1,5 @@
-markdownlint -c markdown-lint.json --disable MD033 -- *.md
+call markdownlint -c markdown-lint.json --disable MD033 -- *.md
+pushd docs-dev
+cd
+call markdownlint -c ..\markdown-lint.json --disable MD033 -- *.md
+popd
